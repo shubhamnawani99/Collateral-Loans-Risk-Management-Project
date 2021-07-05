5c9628d20bcdac4b9bb1ca356153ae53aefd1d64
Shubham Nawani <62939406+shubhamnawani99@users.noreply.github.com>
2021-07-05 19:35:51 +0530

    Add files via upload

---
+++ authorization-service/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into user(id, name, password) values (1, 'admin1', 'abc1');
+insert into user(id, name, password) values (2, 'admin2', 'abc2');
+insert into user(id, name, password) values (3, 'admin3', 'abc3');
+
+
+
+
+insert into customer(id,name,password) values (100011,'alka','alka');
+insert into customer(id,name,password) values (100012,'gautam','gautam');
+insert into customer(id,name,password) values (100013,'rishabh','rishabh');
+insert into customer(id,name,password) values (100014,'shubham','shubham');
+insert into customer(id,name,password) values (100015,'manoj','manoj');
+insert into customer(id,name,password) values (100016,'sarath','sarath');
+insert into customer(id,name,password) values (100017,'samvidha','samvidha');
